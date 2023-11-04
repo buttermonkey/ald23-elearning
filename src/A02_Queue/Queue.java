480beed495fabdf58fe63f86311517e190c3495e
Markus Stoff <s52005@edu.campus02.at>
2023-11-04 23:48:58 +0100

    A02_Queue complete

--- src/A02_Queue/Queue.java
+++ src/A02_Queue/Queue.java
@@ -13,8 +13,11 @@ public class Queue<T>
      * @throws QueueEmptyException 
      */
     public T dequeue() throws QueueEmptyException {
-
-    	return null;
+        if (first == null)
+            throw new QueueEmptyException();
+        Node<T> front = first;
+        first = front.getNext();
+    	return front.getData();
     }
     
     
@@ -24,14 +27,22 @@ public class Queue<T>
      * @param i Zahl
      */
     public void enqueue(T i) {
-
+        Node<T> back = new Node<>(i);
+        if (last != null)
+            last.setNext(back);
+        last = back;
+        if (first == null)
+            first = back;
     }
     
     /**
      * Liefert die Anzahl der Elemente im Stack
-     * @return
+     * @return size of queue
      */
     public int getCount() {
-    	return 0;
+    	int n = 0;
+        for (Node<T> cur = first; cur != null; cur = cur.getNext())
+            n++;
+        return n;
     }
 }
